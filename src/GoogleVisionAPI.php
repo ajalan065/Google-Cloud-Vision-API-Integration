3d5f684a1394b165bce6c1df36948f74067d8e71
ajalan065 <arpitjalan.drupal@gmail.com>
2016-05-28 19:00:56 +0530

    Removed extra services and handled errors primarily

---
+++ src/GoogleVisionAPI.php
@@ -0,0 +1,383 @@
+<?php
+
+namespace Drupal\google_vision;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Component\Serialization\Json;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\ClientInterface;
+
+class GoogleVisionAPI {
+
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * The HTTP client to fetch the feed data with.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  protected $httpClient;
+
+  /**
+   * Stores the API key.
+   *
+   * @var int
+   */
+  protected $apiKey;
+
+  /**
+   * The base url of the Google Cloud Vision API.
+   */
+  const APIEndpoint = 'https://vision.googleapis.com/v1/images:annotate?key=';
+
+  /**
+   * Construct a GoogleVisionAPI object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   *
+   * @param \GuzzleHttp\ClientInterface $http_client
+   *   A Guzzle client object.
+   *
+   * @todo Throw the exception when the api key is not set on status page.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory, ClientInterface $http_client) {
+    $this->configFactory = $config_factory;
+    $this->httpClient = $http_client;
+    $this->apiKey = $this->configFactory->get('google_vision.settings')->get('api_key');
+  }
+
+  /**
+   * Function to retrieve labels for given image.
+   *
+   * @param string $filepath.
+   *
+   * @return Array|bool.
+   */
+  public function labelDetection($filepath) {
+    if (!$this->apiKey) {
+      return FALSE;
+    }
+
+    // It looks pretty dirty. I hope that in future it will be implemented in Google SDK
+    // and we will be able to avoid this approach.
+    $encoded_image = base64_encode(file_get_contents($filepath));
+
+    // Prepare JSON.
+    $data = [
+      'requests' => [
+        [
+          'image' => [
+            'content' => $encoded_image
+          ],
+          'features' => [
+            [
+              'type' => 'LABEL_DETECTION',
+              'maxResults' => 5
+            ],
+          ],
+        ],
+      ],
+    ];
+
+    $url = APIEndpoint . $this->apiKey;
+    $response = $this->httpClient->post($url, [
+      RequestOptions::JSON => $data,
+      RequestOptions::HEADERS => ['Content-Type' => 'application/json'],
+    ]);
+
+    $result = Json::decode($response->getBody());
+    if (empty($result['error'])) {
+      return $result;
+    }
+    return FALSE;
+
+  }
+
+  /**
+   * Function to detect landmarks within a given image.
+   *
+   * @param string $filepath.
+   *
+   * @return Array|bool.
+   */
+  public function landmarkDetection($filepath) {
+    if (!$this->apiKey) {
+      return FALSE;
+    }
+
+    // It looks pretty dirty. I hope that in future it will be implemented in Google SDK
+    // and we will be able to avoid this approach.
+    $encoded_image = base64_encode(file_get_contents($filepath));
+
+    // Prepare JSON.
+    $data = [
+      'requests' => [
+        [
+          'image' => [
+            'content' => $encoded_image
+          ],
+          'features' => [
+            [
+              'type' => 'LANDMARK_DETECTION',
+              'maxResults' => 2
+            ],
+          ],
+        ],
+      ],
+    ];
+
+    $url = APIEndpoint . $this->apiKey;
+    $response = $this->httpClient->post($url, [
+      RequestOptions::JSON => $data,
+      RequestOptions::HEADERS => ['Content-Type' => 'application/json'],
+    ]);
+
+    $result = Json::decode($response->getBody());
+    if (empty($result['error'])) {
+      return $result;
+    }
+    return FALSE;
+
+  }
+
+  /**
+   * Function to detect logos of famous brands within a given image.
+   *
+   * @param string $filepath.
+   *
+   * @return Array|bool.
+   */
+  public function logoDetection($filepath) {
+    if (!$this->apiKey) {
+      return FALSE;
+    }
+
+    // It looks pretty dirty. I hope that in future it will be implemented in Google SDK
+    // and we will be able to avoid this approach.
+    $encoded_image = base64_encode(file_get_contents($filepath));
+
+    // Prepare JSON.
+    $data = [
+      'requests' => [
+        [
+          'image' => [
+            'content' => $encoded_image
+          ],
+          'features' => [
+            [
+              'type' => 'LOGO_DETECTION',
+              'maxResults' => 2
+            ],
+          ],
+        ],
+      ],
+    ];
+
+    $url = APIEndpoint . $this->apiKey;
+    $response = $this->httpClient->post($url, [
+      RequestOptions::JSON => $data,
+      RequestOptions::HEADERS => ['Content-Type' => 'application/json'],
+    ]);
+
+    $result = Json::decode($response->getBody());
+    if (empty($result['error'])) {
+      return $result;
+    }
+    return FALSE;
+
+  }
+
+  /**
+   * Function to detect explicit content within a given image.
+   *
+   * @param string $filepath.
+   *
+   * @return Array|bool.
+   */
+  public function safeSearchDetection($filepath) {
+    if (!$this->apiKey) {
+      return FALSE;
+    }
+
+    // It looks pretty dirty. I hope that in future it will be implemented in Google SDK
+    // and we will be able to avoid this approach.
+    $encoded_image = base64_encode(file_get_contents($filepath));
+
+    // Prepare JSON.
+    $data = [
+      'requests' => [
+        [
+          'image' => [
+            'content' => $encoded_image
+          ],
+          'features' => [
+            [
+              'type' => 'SAFE_SEARCH_DETECTION',
+              'maxResults' => 1
+            ],
+          ],
+        ],
+      ],
+    ];
+
+    $url = APIEndpoint . $this->apiKey;
+    $response = $this->httpClient->post($url, [
+      RequestOptions::JSON => $data,
+      RequestOptions::HEADERS => ['Content-Type' => 'application/json'],
+    ]);
+
+    $result = Json::decode($response->getBody());
+    if (empty($result['error'])) {
+      return $result;
+    }
+    return FALSE;
+
+  }
+
+  /**
+   * Function to retrieve texts for given image.
+   *
+   * @param string $filepath.
+   *
+   * @return Array|bool.
+   */
+  public function opticalCharacterRecognition($filepath) {
+    if (!$this->apiKey) {
+      return FALSE;
+    }
+
+    // It looks pretty dirty. I hope that in future it will be implemented in Google SDK
+    // and we will be able to avoid this approach.
+    $encoded_image = base64_encode(file_get_contents($filepath));
+
+    // Prepare JSON.
+    $data = [
+      'requests' => [
+        [
+          'image' => [
+            'content' => $encoded_image
+          ],
+          'features' => [
+            [
+              'type' => 'TEXT_DETECTION',
+              'maxResults' => 10
+            ],
+          ],
+        ],
+      ],
+    ];
+
+    $url = APIEndpoint . $this->apiKey;
+    $response = $this->httpClient->post($url, [
+      RequestOptions::JSON => $data,
+      RequestOptions::HEADERS => ['Content-Type' => 'application/json'],
+    ]);
+
+    $result = Json::decode($response->getBody());
+    if (empty($result['error'])) {
+      return $result;
+    }
+    return FALSE;
+
+  }
+
+  /**
+   * Function to fetch faces from a given image.
+   *
+   * @param string $filepath.
+   *
+   * @return Array|bool.
+   */
+  public function faceDetection($filepath) {
+    if (!$this->apiKey) {
+      return FALSE;
+    }
+
+    // It looks pretty dirty. I hope that in future it will be implemented in Google SDK
+    // and we will be able to avoid this approach.
+    $encoded_image = base64_encode(file_get_contents($filepath));
+
+    // Prepare JSON.
+    $data = [
+      'requests' => [
+        [
+          'image' => [
+            'content' => $encoded_image
+          ],
+          'features' => [
+            [
+              'type' => 'FACE_DETECTION',
+              'maxResults' => 25
+            ],
+          ],
+        ],
+      ],
+    ];
+
+    $url = APIEndpoint . $this->apiKey;
+    $response = $this->httpClient->post($url, [
+      RequestOptions::JSON => $data,
+      RequestOptions::HEADERS => ['Content-Type' => 'application/json'],
+    ]);
+
+    $result = Json::decode($response->getBody());
+    if (empty($result['error'])) {
+      return $result;
+    }
+    return FALSE;
+
+  }
+
+  /**
+   * Function to retrieve image attributes for given image.
+   *
+   * @param string $filepath.
+   *
+   * @return Array|bool.
+   */
+  public function imageAttributesDetection($filepath) {
+    if (!$this->apiKey) {
+      return FALSE;
+    }
+
+    // It looks pretty dirty. I hope that in future it will be implemented in Google SDK
+    // and we will be able to avoid this approach.
+    $encoded_image = base64_encode(file_get_contents($filepath));
+
+    // Prepare JSON.
+    $data = [
+      'requests' => [
+        [
+          'image' => [
+            'content' => $encoded_image
+          ],
+          'features' => [
+            [
+              'type' => 'IMAGE_PROPERTIES',
+              'maxResults' => 5
+            ],
+          ],
+        ],
+      ],
+    ];
+
+    $url = APIEndpoint . $this->apiKey;
+    $response = $this->httpClient->post($url, [
+      RequestOptions::JSON => $data,
+      RequestOptions::HEADERS => ['Content-Type' => 'application/json'],
+    ]);
+
+    $result = Json::decode($response->getBody());
+    if (empty($result['error'])) {
+      return $result;
+    }
+    return FALSE;
+
+  }
+}
