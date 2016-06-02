54c68fa9d50fdae26f05edaac163d247bb080445
ajalan065 <arpitjalan.drupal@gmail.com>
2016-06-02 12:40:22 +0530

    new tests added

--- src/Tests/ApiKeyConfigurationTest.php
+++ src/Tests/ApiKeyConfigurationTest.php
@@ -2,32 +2,21 @@
 
 namespace Drupal\google_vision\Tests;
 
-use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Url;
 use Drupal\simpletest\WebTestBase;
 
 /**
- * Tests whether API key is set at the admin configuration page.
+ * Tests whether API key is set at the admin configuration page, and whether the
+ * API Key is stored successfully or not.
  *
  * @group google_vision
  */
 class ApiKeyConfigurationTest extends WebTestBase {
 
   /**
-   * Modules to install.
-   *
-   * @var array
+   * {@inheritdoc}
    */
   public static $modules = ['google_vision'];
-
-  /**
-   * Disable strict config schema checking.
-   *
-   * The schema is verified at the end of running the update.
-   *
-   * @var bool
-   */
-  protected $strictConfigSchema = FALSE;
 
   /**
    * A user with permission to create and edit books and to administer blocks.
@@ -51,7 +40,7 @@ class ApiKeyConfigurationTest extends WebTestBase {
   /**
    * Test to implement a runtime requirement checking if API key is not set.
    */
-  function testKeyConfiguration() {
+  public function testKeyConfiguration() {
     $this->drupalGet(Url::fromRoute('google_vision.settings'));
     $this->assertResponse(200);
     $this->assertFieldByName('api_key', '', 'The API key is not set');
@@ -60,23 +49,20 @@ class ApiKeyConfigurationTest extends WebTestBase {
     $this->assertText('Google Vision API key is not set and it is required for some functionalities to work.', 'The content exists on the report page');
     $this->assertLink('Google Vision module settings page', 0, 'Link to the api key configuration page found');
     $this->assertLinkByHref(Url::fromRoute('google_vision.settings')->toString());
-
   }
 
   /**
    * Test to verify that it is stored successfully.
    */
-  function testKeyStorage() {
+  public function testKeyStorage() {
     $this->drupalGet(Url::fromRoute('google_vision.settings'));
     $this->assertResponse(200);
     $this->assertFieldByName('api_key', '', 'The key is not set currently');
-    $apiKey = $this->randomString(40);
-    $data = array(
-      'api_key' => $apiKey,
-    );
-    $this->drupalPostForm(Url::fromRoute('google_vision.settings'), $data, t('Save configuration'));
+    $api_key = $this->randomString(40);
+    $edit = array('api_key' => $api_key);
+    $this->drupalPostForm(Url::fromRoute('google_vision.settings'), $edit, t('Save configuration'));
     $this->drupalGet(Url::fromRoute('google_vision.settings'));
     $this->assertResponse(200);
-    $this->assertFieldByName('api_key', $apiKey, 'The key has been saved');
+    $this->assertFieldByName('api_key', $api_key, 'The key has been saved');
   }
 }
